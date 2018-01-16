b3f49641524c5dfc879c55b8767622a9ccebbba8
campos-97 <joseacampos97@gmail.com>
2018-01-15 21:06:50 -0600

    Initial commit

---
+++ app/src/main/java/com/example/josea/solidify/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.josea.solidify;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    // OpenGL instances.
+    private GLSurfaceView glSurfaceView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        glSurfaceView = new GLSurfaceView(this);
+
+        setContentView(glSurfaceView);
+
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        final boolean supportEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+
+        if (supportEs2){
+            glSurfaceView.setEGLContextClientVersion(2);
+
+            final DisplayMetrics  displayMetrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+
+        } else {
+            return;
+        }
+
+
+        // Example of a call to a native method
+        //TextView tv = (TextView) findViewById(R.id.sample_text);
+        //tv.setText(Integer.toString(getInt()));
+
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        glSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        glSurfaceView.onPause();
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+    public native int getInt();
+}
